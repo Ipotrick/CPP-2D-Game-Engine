2f95d207b25c7115908e3cf2720575b415ed54d2
Patrick Ahrens <pahrens@techfak.de>
2020-03-03 18:47:57 +0100

    korrektur des letzten pushes

--- Spiel/src/Spiel.cpp
+++ Spiel/src/Spiel.cpp
@@ -7,7 +7,7 @@ public:
 	Spiel() : Engine("Test", 1000, 1000) {}
 
 	void update(World& world, double dTime) override {
-		//std::cout << getPerfInfo(2) << std::endl << "iteration: " << getIteration() << std::endl;
+		std::cout << getPerfInfo(2) << std::endl << "iteration: " << getIteration() << std::endl;
 		world.entities.at(0).position.x = sin(getIteration() / 360.0) /2.0;
 		world.entities.at(0).position.y = cos(getIteration() / 360.0) /2.0;
 		world.entities.at(0).rotation = -(getIteration() / 3.14*0.5);
