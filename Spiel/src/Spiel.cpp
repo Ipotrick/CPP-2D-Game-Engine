adb4fa968ccfc85dbd764fd00409bf0c6d4c6e4c
Johannes Kunkel <kunkel.johannes@yahoo.de>
2020-03-07 23:30:47 +0100

    commite jetzt für branches

--- Spiel/src/Spiel.cpp
+++ Spiel/src/Spiel.cpp
@@ -56,7 +56,7 @@ public:
 
 		auto controlledEnt = world.getEntityPtr(controlledEntID);
 
-		//kills entities one contact
+		//kills entities one contact with anything
 		/*for (size_t i = 0; i < world.entities.size(); i++)
 		{
 			if (world.entities[i].isCollided() == true)
