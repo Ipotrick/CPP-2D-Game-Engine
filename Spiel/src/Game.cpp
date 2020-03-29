445cdd3dfb2847c08939dc3e115c15dfaf90416d
Johannes Kunkel <kunkel.johannes@yahoo.de>
2020-03-29 23:56:56 +0200

    merge fixed

--- Spiel/src/Game.cpp
+++ Spiel/src/Game.cpp
@@ -20,65 +20,7 @@ Game::Game() :
 void Game::create() {
 	
 	camera.zoom = 1 / 5.0f;
-
-	vec2 scaleEnt = { 0.4f, 0.8f };
-	auto cEnt = Entity(vec2(0, 0), 0.0f, Collidable(scaleEnt, Form::RECTANGLE, true, vec2(0,0)));
-	auto cDraw = Draw(vec4(0, 0, 0, 1), scaleEnt, 0.6f, Form::RECTANGLE, true);
-	world.spawnSolidEntity(cEnt, cDraw, SolidBody(0.5f, 70.0f));
-	world.addComp<Player>(world.getLastID(), Player());
-	auto playerID = world.getLastID();
-	world.addComp<Composit<4>>(playerID, Composit<4>());
-	
-	auto slaveC = Entity(vec2(0.5, 0), 0.0f, Collidable(vec2(scaleEnt.x / sqrt(2)), Form::RECTANGLE, true, vec2(3, 0)));
-	auto slaveD = Draw(vec4(0, 0, 0, 1), vec2(scaleEnt.x / sqrt(2)), 0.59f, Form::RECTANGLE, true);
-	world.spawnSolidSlave(slaveC, slaveD, playerID, vec2(0, -0.4), 45);
-	world.spawnSolidSlave(slaveC, slaveD, playerID, vec2(0, 0.4), 45);
-
-	vec2 scalePortal = { 28, 28 };
-	Entity portalC = Entity(vec2(-4, -4), 0, Collidable(scalePortal, Form::CIRCLE, true));
-	Draw portalD = Draw(vec4(1, 0, 0, 0.5f), vec2(3, 3), 0.3f, Form::CIRCLE);
-	world.spawnEntity(portalC, portalD);
-	attractorID = world.getLastID(); 
-
-	portalC.size = vec2(3, 3);
-	portalC.position = vec2(4, 4);
-	portalD.color = vec4(0, 0, 1, 0.5f);
-	portalD.drawingPrio = 0.31f;
-	world.spawnEntity(portalC, portalD);
-	pusherID = world.getLastID();
-
-	Entity wallC = Entity(vec2(0,0), 0, Collidable(vec2(0.4f, 10), Form::RECTANGLE, false, vec2(0,0)));
-	Draw wallD = Draw(vec4(0, 0, 0, 1), vec2(0.4f, 10), 0.5f, Form::RECTANGLE, true);
-	for (int i = 0; i < 4; i++) {
-		float rotation = 90.0f * i;
-		wallC.position = rotate(vec2(-5.f, 0), rotation);
-		wallC.rotation = rotation;
-		world.spawnSolidEntity(wallC, wallD, SolidBody(0.3f, 1'000'000'000'000'000.0f));
-	}
-
-	int num = 500;
-
-	vec2 scale = vec2(0.05f, 0.05f);
-	Entity trashEntC = Entity(vec2(0, 0), 0.0f, Collidable(scale, Form::CIRCLE, true, vec2(0,0)));
-	//trashEntC.particle = true;
-	Draw trashEntD = Draw(vec4(0,0, 0, 1), scale, 0.5f, Form::CIRCLE, true);
-	auto slaveEntC = trashEntC;
-	slaveEntC.form = Form::RECTANGLE;
-	auto slaveEntD = trashEntD;
-	slaveEntD.color = vec4(0,0,0, 1);
-	slaveEntD.drawingPrio += 0.01f;
-	slaveEntD.form = Form::RECTANGLE;
-	auto trashSolid = SolidBody(0.1f, 0.5f);
-	trashSolid.momentOfInertia = 0.005f;
-	for (int i = 0; i < num; i++) {
-
-		trashEntC.position = { static_cast<float>(rand() % 1000 / 500.0f - 1.0f) * 4.6f, static_cast<float>(rand() % 1000 / 500.0f - 1.0f) * 4.6f };
-
-		world.spawnSolidEntity(trashEntC, trashEntD, trashSolid);
-		world.addComp<Health>(world.getLastID(), Health(100));
-		world.addComp<Composit<4>>(world.getLastID(), Composit<4>());
-		world.spawnSolidSlave(slaveEntC, slaveEntD, world.getLastID(), vec2(0.025f, 0), 0);
-	}
+	world.loadMap("shit");
 }
 
 void Game::update(World& world, float deltaTime) {
@@ -128,34 +70,6 @@ void Game::update(World& world, float deltaTime) {
 
 	//display performance statistics
 	//std::cout << getPerfInfo(5) << '\n';
-	
-	auto attractor = world.getEntityPtr(attractorID);
-	auto pusher = world.getEntityPtr(pusherID);
-	float acceleration = 0.1f;
-	float minDist = -attractor->getRadius() + 0.2f;
-
-	auto [begin, end] = getCollisionInfos(attractorID);
-	if (begin != end) {
-		for (auto iter = begin; iter != end; iter++) {
-			auto otherPtr = world.getEntityPtr(iter->idB);
-			if (otherPtr->isSolid() && otherPtr->isDynamic()) {
-				otherPtr->velocity += normalize(attractor->getPos() - otherPtr->getPos()) * acceleration * powf((iter->clippingDist / attractor->getRadius()), 2) * deltaTime;
-				if (iter->clippingDist < minDist) {
-					vec2 otherToCenter = (attractor->position + pusher->position)/2.0f - otherPtr->getPos();
-					otherPtr->position += otherToCenter * 2;
-				}
-			}
-		}
-	}
-	auto [begin2, end2] = getCollisionInfos(pusherID);
-	if (begin2 != end2) {
-		for (auto iter = begin2; iter != end2; iter++) {
-			auto otherPtr = world.getEntityPtr(iter->idB);
-			if (otherPtr->isSolid()) {
-				otherPtr->velocity += normalize(pusher->getPos() - otherPtr->getPos()) * -acceleration * powf((iter->clippingDist / pusher->getRadius()), 2) * deltaTime;
-			}
-		}
-	}
 }
 
 void Game::cursorManipFunc()
